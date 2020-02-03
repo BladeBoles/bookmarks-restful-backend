82fae23b5ca64dcc9ad1bb2edfa1b8bd305a0627
Blade Boles <bladerboles@gmail.com>
2020-02-03 15:03:53 -0700

    /api prefix added and tests passing

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PORT: 8080,
+  PORT: 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN || 12345,
   DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/bookmarks',
